0f00574819e4f7172562349449c6388dc8f7e2a2
gebra04 <gebrafacul@gmail.com>
2025-05-21 13:40:46 -0300

    Created distance bipartite graph

--- Metodos/uteis.py
+++ Metodos/uteis.py
@@ -277,3 +277,34 @@ def funcao_objetivo(problema: Processa.Problema, itensP: dict, itensC: dict) -> 
             return 0
     # Retornando a soma.
     return soma
+
+
+def peso_aresta(problema, corredor_id, pedido_id):
+    """
+    Peso = quantidade de itens faltantes para suprir o pedido.
+    Retorna 9999 se mais de 15 unidades estiverem faltando.
+    """
+    oferta = problema.aisles[corredor_id]
+    demanda = problema.orders[pedido_id]
+    faltantes = 0
+
+    for item, qnt in demanda.items():
+        faltantes += max(0, qnt - oferta.get(item, 0))
+
+    return faltantes
+
+
+def inicia_grafo(problema):
+    """
+    Inicializa o grafo bipartido entre corredores e pedidos.
+    Cada aresta tem peso igual à quantidade de itens faltantes para suprir o pedido.
+    """
+    grafo = defaultdict(list)
+    for c_id in range(len(problema.aisles)):
+        for p_id in range(len(problema.orders)):
+            peso = peso_aresta(problema, c_id, p_id)
+            grafo[c_id].append((p_id, peso))
+        
+        grafo[c_id].sort(key=lambda x: x[1])
+
+    return grafo
